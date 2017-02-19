b6ab12e26884e2e6dc1a0d2be445151adb071223
electrobas94 <electrobas94@gmail.com>
2017-02-19 20:34:28 +0300

    Test template interfase

--- CoSyLa/CoSyLa/Scripts/App/InstrumentEditor/InstrumentEditor.ts
+++ CoSyLa/CoSyLa/Scripts/App/InstrumentEditor/InstrumentEditor.ts
@@ -1,31 +1,50 @@
 ﻿class InstrumentEditor
 {
 	 w: TabWidget;
-    constructor() {
+    constructor()
+	 {
+
         this.w = new TabWidget(document.getElementById("mainWindow"));
 		  this.w.AddTab("Визуальный редактор");
 		  this.w.AddTab("Редактор логики");
 		  this.w.AddTab("Общие настройки");
 
-		  let but = new ButtonWidget();
-		  but.Text = "Жмякни";
+		  let w: Widget = new Widget();
+		  let w2: Widget = new Widget();
 
-		  let butGroup: ButtonGroupWidget = new ButtonGroupWidget();
-		  butGroup.AddButton();
-		  butGroup.AddButton();
-		  butGroup.AddButton();
+		  this.w.AddWidgetOnTab(0, w);
+		  this.w.AddWidgetOnTab(0, w2);
 
-		  let butGroup2: ButtonGroupWidget = new ButtonGroupWidget();
-		  butGroup2.AddButton();
-		  butGroup2.AddButton();
-		  butGroup2.AddButton();
+		  w.Width = 77;
+		  w2.Width = 23;
+		  w.Height = 100;
+		  w2.Height = 100; 
 
-		  let toolBar: ToolBarWidget = new ToolBarWidget();
-		  toolBar.AddToolGroup(butGroup);
-		  toolBar.AddToolGroup(butGroup2);
+		  let tab: TabWidget = new TabWidget(w2.DomElement);
+		  tab.AddTab("Элементы");
+		  tab.AddTab("Свойства");
+		  tab.AddTab("Инспектор");
 
-		  //this.w.AddWidgetOnTab(0, but);
-		  this.w.AddWidgetOnTab(0, toolBar);
+		  let toolBar: ToolBarWidget = new ToolBarWidget(w.DomElement);
+		  toolBar.Height = 5;
+
+		  toolBar.AddToolGroup();
+		  toolBar.AddToolGroup();
+
+		  toolBar.ToolGroup[0].AddButton();
+		  toolBar.ToolGroup[0].AddButton();
+		  toolBar.ToolGroup[0].AddButton();
+
+		  toolBar.ToolGroup[1].AddButton();
+		  toolBar.ToolGroup[1].AddButton();
+		  toolBar.ToolGroup[1].AddButton();
+
+		  let canvas: Widget = new Widget();
+		  canvas.Height = 95;
+		  canvas.DomElement.style.backgroundColor = "#c5c5c5";
+		  w.AddChildWidget(canvas);
+
+		  this.w.Height = 100;
     }
 }
 
