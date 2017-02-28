bbdfaa8420c933f58233a5b784a9fc648df041e8
electrobas94 <electrobas94@gmail.com>
2017-03-01 01:42:14 +0300

    Added description (data-model) for Model 3d

--- CoSyLa/CoSyLa/Models/Model3D.cs
+++ CoSyLa/CoSyLa/Models/Model3D.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace CoSyLa.Models
 {
 	 public class Model3D
 	 {
+		  public ObjectId IdModel { get; set; }
+		  public string NameModel { get; set; }
+
+		  public ObjectId IdMeshFileJson { get; set; }
+		  public ObjectId IdMeshFileBin  { get; set; }
+
+		  public List<ObjectId> TextureFiles { get; set; }
 	 }
 }
